c6efed60ada28c0ecbddfbc5a44a49890934b9e6
Christophe Vila <christophe.vila@gemalto.com>
2020-03-14 15:54:22 +0100

    allow remote installation (prepare for helm 3)

---
+++ scripts/install_plugin.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -e
+
+# shellcheck disable=SC2002
+version="$(cat plugin.yaml | grep "version" | cut -d '"' -f 2)"
+echo "Downloading and installing spray v${version} ..."
+
+url=""
+if [ "$(uname)" = "Linux" ] ; then
+    url="https://github.com/ThalesGroup/helm-spray/releases/download/v${version}/helm-spray-linux-amd64.tar.gz"
+else
+    url="https://github.com/ThalesGroup/helm-spray/releases/download/v${version}/helm-spray-windows-amd64.tar.gz"
+fi
+
+echo "$url"
+
+# Download with curl if possible.
+# shellcheck disable=SC2230
+if [ -x "$(which curl 2>/dev/null)" ]; then
+    curl -sSL "${url}" -o "releases/v${version}.tar.gz"
+else
+    wget -q "${url}" -O "releases/v${version}.tar.gz"
+fi
+tar xzf "releases/v${version}.tar.gz" -C "releases/v${version}"
+mv "releases/v${version}/helm-spray" "helm-spray" || \
+    mv "releases/v${version}/helm-spray.exe" "helm-spray"
+mv "releases/v${version}/plugin.yaml" .
+mv "releases/v${version}/README.md" .
+mv "releases/v${version}/LICENSE" .
