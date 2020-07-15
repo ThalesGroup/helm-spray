16d0eabac234bccd49d895a249bd8f85cde41a16
pamiel <14542297+pamiel@users.noreply.github.com>
2020-07-15 18:22:27 +0200

    Do not wait for StatefulSet Pods re-deployment when StatefulSet update strategy is OnDelete

--- pkg/kubectl/kubectl.go
+++ pkg/kubectl/kubectl.go
@@ -117,11 +117,6 @@ func IsJobCompleted(job string, namespace string) bool {
 // GetStatefulSetStrategy
 func GetStatefulSetStrategy(statefulset string, namespace string) string {
 	s := getObjectDescriptionItem("statefulset", statefulset, namespace, ".spec.updateStrategy.type")
-//	if len(s) >= 2 {
-//		if s[0] == `'` && s[len(s)-1] == `'` {
-//			return s[1 : len(s)-1]
-//		}
-//	}
 	return strings.Trim(s, "'")
 }
 
